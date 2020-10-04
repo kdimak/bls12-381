45cf2399727866eeb6bcc6a0a7b390ddb0c584a1
Dmitriy Kinoshenko <dkinoshenko@gmail.com>
2020-10-04 09:43:31 +0300

    test: completed development of osswuMap. TODO sign check.

--- g1.go
+++ g1.go
@@ -5,8 +5,6 @@ import (
 	"hash"
 	"math"
 	"math/big"
-
-	"fmt"
 )
 
 // PointG1 is type for point in G1.
@@ -543,12 +541,7 @@ func (g *G1) HashToCurve(f func() hash.Hash, msg, domain []byte) (*PointG1, erro
 	}
 	u0, u1 := hashRes[0], hashRes[1]
 
-	g.osswuExp(u0)
-
-	x0, y0 := swuMapG1(u0)
-	x1, y1 := swuMapG1(u1)
-	one := new(fe).one()
-	p0, p1 := &PointG1{*x0, *y0, *one}, &PointG1{*x1, *y1, *one}
+	p0, p1 := osswuMap(u0), osswuMap(u1)
 	g.Add(p0, p0, p1)
 	g.Affine(p0)
 	isogenyMapG1(&p0[0], &p0[1])
@@ -556,15 +549,16 @@ func (g *G1) HashToCurve(f func() hash.Hash, msg, domain []byte) (*PointG1, erro
 	return g.Affine(p0), nil
 }
 
-var XI = &fe{0x886c00000023ffdc,
-	0xf70008d3090001d,
-	0x77672417ed5828c3,
-	0x9dac23e943dc1740,
-	0x50553f1b9c131521,
-	0x78c712fbe0ab6e8,
+var SQRT_M_XI_CUBED = &fe{
+	0x43b571cad3215f1f,
+	0xccb460ef1c702dc2,
+	0x742d884f4f97100b,
+	0xdb2c3e3238a3382b,
+	0xe40f3fa13fce8f88,
+	0x73a2af9892a2ff,
 }
 
-func (g *G1) osswuExp(u *fe) {
+func osswuMap(u *fe) *PointG1 {
 	var params = swuParamsForG1
 
 	usq := new(fe)
@@ -622,24 +616,425 @@ func (g *G1) osswuExp(u *fe) {
 		square(tmp2, gx0_den)
 		mul(tmp2, tmp2, tmp1)
 
-		// TODO add chain
-		//  as a result, tmp2 is changed to
-		//  remove this hardcode
-		*tmp2 = fe{
-			9603680354614924412,
-			983746686070132455,
-			7958276960564301894,
-			7034158915266105779,
-			6974467999659521750,
-			183843923333619465,
-		}
+		tmp3 := new(fe)
+		*tmp3 = *tmp2
+
+		chain_pm3div4(tmp2, tmp3)
 
 		mul(tmp2, tmp2, tmp1)
 
 		return tmp2
 	}()
 
-	//test_cand := new(fe)
+	test_cand := new(fe)
+	square(test_cand, sqrt_candidate)
+	mul(test_cand, test_cand, gx0_den)
 
-	fmt.Printf("sqrt_candidate: %v", sqrt_candidate)
+	x_num, y := new(fe), new(fe)
+	if test_cand.equal(gx0_num) {
+		x_num = x0_num
+		y = sqrt_candidate
+	} else {
+		mul(x_num, x0_num, xi_usq)
+
+		mul(y, usq, u)
+		mul(y, y, sqrt_candidate)
+		mul(y, y, SQRT_M_XI_CUBED)
+	}
+
+	// todo check negate
+	if y.signBE() != u.signBE() {
+		neg(y, y)
+	}
+
+	mul(x_num, x_num, x0_den)
+	mul(y, y, gx0_den)
+
+	return &PointG1{
+		*x_num,
+		*y,
+		*x0_den,
+	}
+}
+
+func chain_pm3div4(tmpvar1, tmpvar0 *fe) {
+	square(tmpvar1, tmpvar0)
+
+	tmpvar9 := new(fe)
+	mul(tmpvar9, tmpvar1, tmpvar0)
+
+	tmpvar5 := new(fe)
+	square(tmpvar5, tmpvar1)
+
+	tmpvar2 := new(fe)
+	mul(tmpvar2, tmpvar9, tmpvar1)
+
+	tmpvar7 := new(fe)
+	mul(tmpvar7, tmpvar5, tmpvar9)
+
+	tmpvar10 := new(fe)
+	mul(tmpvar10, tmpvar2, tmpvar5)
+
+	tmpvar13 := new(fe)
+	mul(tmpvar13, tmpvar7, tmpvar5)
+
+	tmpvar4 := new(fe)
+	mul(tmpvar4, tmpvar10, tmpvar5)
+
+	tmpvar8 := new(fe)
+	mul(tmpvar8, tmpvar13, tmpvar5)
+
+	tmpvar15 := new(fe)
+	mul(tmpvar15, tmpvar4, tmpvar5)
+
+	tmpvar11 := new(fe)
+	mul(tmpvar11, tmpvar8, tmpvar5)
+
+	tmpvar3 := new(fe)
+	mul(tmpvar3, tmpvar15, tmpvar5)
+
+	tmpvar12 := new(fe)
+	mul(tmpvar12, tmpvar11, tmpvar5)
+
+	tmpvar14 := new(fe)
+	mul(tmpvar14, tmpvar12, tmpvar5)
+
+	square(tmpvar1, tmpvar4)
+
+	tmpvar6 := new(fe)
+	mul(tmpvar6, tmpvar1, tmpvar9)
+
+	mul(tmpvar5, tmpvar1, tmpvar2)
+
+	for i := 0; i < 12; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar15)
+
+	for i := 0; i < 7; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar8)
+
+	for i := 0; i < 4; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar2)
+
+	for i := 0; i < 6; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar7)
+
+	for i := 0; i < 7; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar12)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar5)
+
+	for i := 0; i < 2; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar9)
+
+	for i := 0; i < 6; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar4)
+
+	for i := 0; i < 6; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar4)
+
+	for i := 0; i < 6; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar10)
+
+	for i := 0; i < 3; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar9)
+
+	for i := 0; i < 7; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar4)
+
+	for i := 0; i < 4; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar4)
+
+	for i := 0; i < 6; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar8)
+
+	for i := 0; i < 6; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar14)
+
+	for i := 0; i < 3; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar0)
+
+	for i := 0; i < 8; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar4)
+
+	for i := 0; i < 7; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar12)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar13)
+
+	for i := 0; i < 6; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar4)
+
+	for i := 0; i < 6; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar6)
+
+	for i := 0; i < 4; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar10)
+
+	for i := 0; i < 8; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar6)
+
+	for i := 0; i < 4; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar4)
+
+	for i := 0; i < 7; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar12)
+
+	for i := 0; i < 9; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar11)
+
+	for i := 0; i < 2; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar9)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar7)
+
+	for i := 0; i < 7; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar2)
+
+	for i := 0; i < 7; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar10)
+
+	for i := 0; i < 6; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar12)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar6)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar11)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar11)
+
+	for i := 0; i < 8; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar4)
+
+	for i := 0; i < 7; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar3)
+
+	for i := 0; i < 9; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar8)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar4)
+
+	for i := 0; i < 3; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar9)
+
+	for i := 0; i < 8; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar8)
+
+	for i := 0; i < 3; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar9)
+
+	for i := 0; i < 7; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar10)
+
+	for i := 0; i < 9; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar8)
+
+	for i := 0; i < 6; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar3)
+
+	for i := 0; i < 6; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar5)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar5)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar5)
+
+	for i := 0; i < 4; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar4)
+
+	for i := 0; i < 3; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar9)
+
+	for i := 0; i < 8; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar3)
+
+	for i := 0; i < 7; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar5)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar5)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar5)
+
+	for i := 0; i < 4; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar8)
+
+	for i := 0; i < 4; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar7)
+
+	for i := 0; i < 7; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar5)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar6)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar5)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar5)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar5)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar5)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar5)
+
+	for i := 0; i < 5; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar5)
+
+	for i := 0; i < 4; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar4)
+
+	for i := 0; i < 6; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar3)
+
+	for i := 0; i < 4; i++ {
+		square(tmpvar1, tmpvar1)
+	}
+	mul(tmpvar1, tmpvar1, tmpvar2)
+
+	square(tmpvar1, tmpvar1)
 }
